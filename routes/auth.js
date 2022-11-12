13ae461dd11df3820f275bcb44e6e1a000565e02
Lovish Tater <lovishtater@gmail.com>
2022-11-12 21:09:00 +0530

    test

--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,17 @@ const {
   signup,
   signin,
   signout,
+  isSignedIn,
   userSignupValidator,
   userSigninValidator,
 } = require("../controllers/auth");
 
 
 // Routes
-router.post("/addUser", userSignupValidator,  signup);
-router.post("/loginUser", userSigninValidator,  signin);
+router.post("/addUser",  userSignupValidator, signup);
+router.post("/login", userSigninValidator, signin);
 router.get("/signout", signout);
-
+router.get("/testroute", isSignedIn, (req, res) => {
+  res.send("A protected route");
+});
 module.exports = router;
